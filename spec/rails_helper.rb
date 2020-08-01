d1f07ea61f28345f3183092bd1142d47b2d6bf93
Derek Borski <derek.borski@gmail.com>
2020-08-01 14:44:05 -0600

    Add new fixture files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -73,5 +73,8 @@ RSpec.configure do |config|
     config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
     config.hook_into :webmock
     config.configure_rspec_metadata!
+    config.filter_sensitive_data('<OPEN_WEATHER_API_KEY>') { ENV['OPEN_WEATHER_API_KEY'] }
+    config.filter_sensitive_data('<MAPQUEST_API_KEY>') { ENV['MAPQUEST_API_KEY'] }
+    config.filter_sensitive_data('<UNSPLASH_API_KEY>') { ENV['UNSPLASH_API_KEY'] }
   end
 end
