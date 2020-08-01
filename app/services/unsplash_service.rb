9bf9b7f611cfe8ffd91f87275f5a6ecce1e216e0
Derek Borski <derek.borski@gmail.com>
2020-08-01 12:17:56 -0600

    Create unsplash_service and get_photos_by_keyword method

---
+++ app/services/unsplash_service.rb
@@ -0,0 +1,23 @@
+class UnsplashService 
+
+  def get_photos_by_keyword(keyword)
+    params = { query: keyword }
+
+    get_json('search/photos', params)
+  end
+  
+  private
+
+  def get_json(url, params)
+    response = conn.get(url, params) do |request|
+      request.headers['Authorization'] = "Client-ID " + ENV['UNSPLASH_API_KEY']
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: 'https://api.unsplash.com/') do |f|
+      f.adapter Faraday.default_adapter
+    end
+  end
+end
