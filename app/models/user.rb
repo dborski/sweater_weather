4e958363f2b84de6e542b40cbf17c5b35f581634
Derek Borski <derek.borski@gmail.com>
2020-08-01 23:29:26 -0600

    Update validations on user model and add before_crate method to set api_key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
-  validates :api_key, uniqueness: true, presence: true
-
+  # validates_presence_of :api_key
+  validates_presence_of :password, require: true, confirmation: true
   validates_confirmation_of :password
   has_secure_password
+  
+  before_create do 
+    self.api_key = SecureRandom.uuid
+  end 
 end
