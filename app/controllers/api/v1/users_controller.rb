7e8432d0e73debb2bf10005b2c45f394f9a79183
Derek Borski <derek.borski@gmail.com>
2020-08-01 23:28:48 -0600

    Create api/v1/users_controller along with index action.

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::UsersController < ApplicationController
+  protect_from_forgery prepend: true
+
+  def create
+    user = User.new(user_params)
+    if user.save && passwords_match?
+      render json: UsersSerializer.new(user)
+    else 
+      msg = { body: user.errors.full_messages.to_sentence, status: 400 }
+      render json: msg
+    end 
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end 
+
+  def passwords_match?
+    params[:password] == params[:password_confirmation]
+  end 
+end
