4ca09eafc0091673908e1f30b653f9b16d0ca002
Derek Borski <derek.borski@gmail.com>
2020-08-01 18:18:42 -0600

    Add get_single_photo_by_keyword method and spec

--- spec/services/unsplash_service_spec.rb
+++ spec/services/unsplash_service_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 describe 'Unsplash API' do
   before(:each) do
-    keyword = 'denver'
+    keyword = 'denver,co'
 
     unsplash = UnsplashService.new
     @unsplash_photos = unsplash.get_photos_by_keyword(keyword)
+    @photo = unsplash.get_single_photo_by_keyword(keyword)
   end
 
   it 'sends photos by keyword search', :vcr do
@@ -26,5 +27,25 @@ describe 'Unsplash API' do
     expect(@unsplash_photos[:results].first[:links]).to have_key(:html)
     expect(@unsplash_photos[:results].first[:links]).to have_key(:download)
     expect(@unsplash_photos[:results].first[:links]).to have_key(:download_location)
+  end
+
+  it 'gets a single photo', :vcr do
+    expect(@photo).to be_a(Hash)
+    expect(@photo).to have_key(:id)
+    expect(@photo).to have_key(:created_at)
+    expect(@photo).to have_key(:updated_at)
+    expect(@photo).to have_key(:width)
+    expect(@photo).to have_key(:height)
+    expect(@photo).to have_key(:color)
+    expect(@photo).to have_key(:description)
+    expect(@photo).to have_key(:alt_description)
+    expect(@photo[:urls]).to have_key(:raw)
+    expect(@photo[:urls]).to have_key(:full)
+    expect(@photo[:urls]).to have_key(:regular)
+    expect(@photo[:urls]).to have_key(:thumb)
+    expect(@photo[:links]).to have_key(:self)
+    expect(@photo[:links]).to have_key(:html)
+    expect(@photo[:links]).to have_key(:download)
+    expect(@photo[:links]).to have_key(:download_location)
   end
 end
