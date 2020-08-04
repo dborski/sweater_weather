47b0b87244b256cb3f61a5b095a64f41011e7f99
Derek Borski <derek.borski@gmail.com>
2020-08-04 11:00:07 -0600

    Customize updated_at attribute in serializer to display time a little more clearly

--- app/serializers/road_trip_serializer.rb
+++ app/serializers/road_trip_serializer.rb
@@ -1,6 +1,10 @@
 class RoadTripSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :origin, :destination, :travel_time, :arrival_temp, :arrival_weather_desc, :updated_at
+  attributes :name, :origin, :destination, :travel_time, :arrival_temp, :arrival_weather_desc
+
+  attribute :updated_at do |trip|
+    trip.updated_at.strftime("%c %:z")
+  end 
 
   belongs_to :user
 end
