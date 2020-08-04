b601852281996b808c2ef990ef23b027bff190ca
Derek Borski <derek.borski@gmail.com>
2020-08-04 03:12:32 -0600

    Working version create action. Need to refactor

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,10 +1,24 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-  user_api_key = params[:api_key]
-  origin = params[:origin]
-  destination = params[:destination]
+    user = User.find_by(api_key: road_trip_params[:api_key])
 
-  road_trip = RoadTripCreator.new(user_api_key, origin, destination).create_road_trip
-  # render json: RoadTripSerializer.new(road_trip)
+    if authorized_user?(user)
+      road_trip = RoadTripCreator.new(road_trip_params).create_road_trip
+      options = { include: [:user], fields: { user: [:api_key] } }
+      render json: RoadTripSerializer.new(road_trip, options)
+    else 
+      msg = { body: 'Unauthorized', status: 401 }
+      render json: msg
+    end
   end
+
+  private
+
+  def authorized_user?(user)
+    user && road_trip_params[:api_key]
+  end 
+
+  def road_trip_params
+    params.permit(:api_key, :origin, :destination)
+  end 
 end
