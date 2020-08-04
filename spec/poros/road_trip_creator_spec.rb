e7b177ba083eea32f0eaae116eb41acb5fc20e80
Derek Borski <derek.borski@gmail.com>
2020-08-04 01:41:16 -0600

    Update spec with new helper methods

--- spec/poros/road_trip_creator_spec.rb
+++ spec/poros/road_trip_creator_spec.rb
@@ -18,11 +18,23 @@ describe 'RoadTrip' do
   it 'attributes', :vcr do
     expect(@rt_creator.origin).to eq(@origin)
     expect(@rt_creator.destination).to eq(@destination)
+  end
+
+  it 'Can get a forecast for the destination', :vcr do
+    expect(@rt_creator.forecast).to have_key(:temperature)
+    expect(@rt_creator.forecast).to have_key(:summary)
+  end 
+
+  it 'Can create a name for the road_trip', :vcr do
+    expect(@rt_creator.forecast).to have_key(:temperature)
+    expect(@rt_creator.forecast).to have_key(:summary)
   end 
 
   it 'Creates a user road_trip', :vcr do
+    @rt_creator.create_road_trip
+    
     expect(@user.road_trips.first.origin).to eq(@origin)
-    expect(@user.road_trips.first.destination).to eq(@desintation)
+    expect(@user.road_trips.first.destination).to eq(@destination)
     expect(@user.road_trips.first.travel_time).to_not eq(nil)
     expect(@user.road_trips.first.arrival_temp).to_not eq(nil)
     expect(@user.road_trips.first.arrival_weather_desc).to_not eq(nil)
