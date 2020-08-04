c1d9e1173c7f2eca1ac3067f43a9ebe384413b11
Derek Borski <derek.borski@gmail.com>
2020-08-04 04:14:19 -0600

    Add after_find method to update weather and travel time each time a road trip is called from the database

--- app/models/road_trip.rb
+++ app/models/road_trip.rb
@@ -1,3 +1,12 @@
 class RoadTrip < ApplicationRecord
+  include ServicesHelper
+
   belongs_to :user
+
+  after_find do |trip|
+    weather = get_geocoded_weather(destination)
+    trip.travel_time = get_directions(origin, destination)[:route][:formattedTime],
+    trip.arrival_temp = weather[:current][:temp],
+    trip.arrival_weather_desc = weather[:current][:weather].first[:description]
+  end 
 end
