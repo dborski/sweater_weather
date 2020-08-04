f7b55cd420b5f345730d4121ad81044cea4f1aab
Derek Borski <derek.borski@gmail.com>
2020-08-04 00:10:55 -0600

    Rename to backgrounds_controller

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::BackgroundController < ApplicationController
+class Api::V1::BackgroundsController < ApplicationController
   def index
     unsplash_photo = UnsplashService.new.get_single_photo_by_keyword(params[:location])
     render json: ImageSerializer.new(Image.new(unsplash_photo))
