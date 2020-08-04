5ab42395a366acbeeeb7218727a452d1c71f8484
Derek Borski <derek.borski@gmail.com>
2020-08-04 01:54:41 -0600

    Update controller with successful road_trip_creator

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,27 +1,10 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-  #   user = User.find(params[:api_key])
-  #   origin = params[:origin]
-  #   destination = params[:destination]
+  user_api_key = params[:api_key]
+  origin = params[:origin]
+  destination = params[:destination]
 
-  #   mapquest_service = MapquestService.new
-  #   # most likely need to convert travel time
-  #   travel_time = mapquest_service.get_directions(origin, destination)[:route][:formatted_time]
-  #   address = mapquest_service.get_geocode_address(destination)
-  #   lat = address[:results].first[:locations].first[:latLng][:lat]
-  #   lng = address[:results].first[:locations].first[:latLng][:lng]
-  #   weather_data = OpenweatherService.new.get_weather_by_location(lat, lng)
-
-  #   destination_forecast = Forecast.new(weather_data)
-
-  #   user.road_trip.create(
-  #     origin: origin,
-  #     destination: destination,
-  #     travel_time: travel_time,
-  #     arrival_forecast: destination_forecast.current
-  #   )
-
-  road_trip = RoadTripCreator.new(user_api_key, origin, destination)
-  render json: RoadTripSerializer.new(road_trip)
+  road_trip = RoadTripCreator.new(user_api_key, origin, destination).create_road_trip
+  # render json: RoadTripSerializer.new(road_trip)
   end
 end
