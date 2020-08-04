0ac59ad1e66eec28d916b4c33ba8f3f687af5c0f
Derek Borski <derek.borski@gmail.com>
2020-08-04 00:11:25 -0600

    Add road_trip route and refactor all other routes to use resources instead of handrolled

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,19 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get '/forecast', to: 'forecast#index'
-      get '/backgrounds', to: 'background#index'
+      resources :forecast, only: [:index]
 
-      post '/users', to: 'users#create'
-      post '/sessions', to: 'sessions#create'
+      resources :backgrounds, only: [:index]
+
+      resources :sessions, only: [:create]
+
+      resources :users, only: [:create]
+
+      post '/road_trip', to: 'road_trip#create'
+      
+      
+
+      # post '/users/:id/road_trips', to: 'road_trips#create'
     end
   end
 end
